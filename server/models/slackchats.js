db68bf455154b58e8257797066e3e2e69cd1bde0
jojunmyeong <a8456452@naver.com>
2020-01-22 15:42:40 +0900

    새로운 디비 파일들

---
+++ server/models/slackchats.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+ 
+    const Slack = sequelize.define("slackchat", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+
+    //Slack.associate = function(models) {
+        //models.board.hasMany(models.group);
+        //models.slack.belongsTo(models.group);
+    //};
+ 
+    return Slack;
+};
---
+++ server/server_config_empty.js
@@ -0,0 +1,13 @@
+var configs = {
+    database : "",
+    user : "",
+    password : "",
+    host : "",
+    dialect : "",
+
+    b_token : "",
+    p_token : "",
+    bearer_p_token : "",
+}
+
+module.exports = configs;
