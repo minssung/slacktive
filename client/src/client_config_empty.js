834e7993b4a69b6d9cefe03562276d78a4acb9c9
jojunmyeong <a8456452@naver.com>
2020-07-30 16:26:19 +0900

    필요없는 모듈 삭제 및 필요없는 리소스 파일 삭제

--- client/src/client_config_empty.js
+++ client/src/client_config_empty.js
@@ -1,13 +1,6 @@
 var client_c = {
-    dataTitleReg : "",
-    dataTimeReg : "",
-    dataCateReg : "",
-    dataCateTimeReg : "",
-
     channel_calendar : "",
     channel_times : "",
-
-    colors : [''],
 }
 
 module.exports = client_c;
